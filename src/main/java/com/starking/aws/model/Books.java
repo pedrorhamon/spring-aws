1e0dc0a0fe82a6eb3c7cb81629f01b3368970545
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-02-10 16:45:56 -0300

    adicionando as migrations

---
+++ src/main/java/com/starking/aws/model/Books.java
@@ -0,0 +1,13 @@
+package com.starking.aws.model;
+
+import java.io.Serializable;
+
+/**
+ * @author pedroRhamon
+ */
+
+public class Books implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
