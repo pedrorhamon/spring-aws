83d70f900dd4033e7ac86818df0ab7ff529a9b79
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-02-10 16:28:09 -0300

    inicializando projeto em aws e spring 3.0

---
+++ src/test/java/com/starking/aws/AwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starking.aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
