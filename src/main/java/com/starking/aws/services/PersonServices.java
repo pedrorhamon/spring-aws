d032be181cfc1d788ad142dedc4b779a4443fba2
Pedro Rhamon <pedrorhamon16@gmail.com>
2024-02-12 15:20:21 -0300

    adicionando pagination nas apis

--- src/main/java/com/starking/aws/services/PersonServices.java
+++ src/main/java/com/starking/aws/services/PersonServices.java
@@ -1,10 +1,10 @@
 package com.starking.aws.services;
 
-import java.util.List;
-
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.starking.aws.converter.DozerConverter;
@@ -25,9 +25,19 @@ public class PersonServices {
 		return vo;
 	}
 	
-	public List<PersonVO> findAll() {
-		return DozerConverter.parseListObjects(repository.findAll(), PersonVO.class);
-	}	
+	public Page<PersonVO> findPersonByName(String firstName, Pageable pageable) {
+		var page = repository.findPersonByName(firstName, pageable);
+		return page.map(this::convertToPersonVO);
+	}
+	
+	public Page<PersonVO> findAll(Pageable pageable) {
+		var page = repository.findAll(pageable);
+		return page.map(this::convertToPersonVO);
+	}
+	
+	private PersonVO convertToPersonVO(Person entity) {
+		return DozerConverter.parseObject(entity, PersonVO.class);
+	}
 	
 	public PersonVO findById(Long id) {
 
